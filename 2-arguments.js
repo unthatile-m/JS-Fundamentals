316f672bb193fd1124f77141a5f32305d171a2fe
unthatile makhajane <unthatile.alx@yahoo.com>
2025-09-14 03:00:25 +0200

    Add 2-arguments.js script

---
+++ 2-arguments.js
@@ -0,0 +1,11 @@
+// 2-arguments.js
+
+const args = process.argv.slice(2); // Get arguments passed after "node 2-arguments.js"
+
+if (args.length === 0) {
+    console.log("No argument");
+} else if (args.length === 1) {
+    console.log("Argument found");
+} else {
+    console.log("Arguments found");
+}
