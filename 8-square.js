e118215bdeac36fce48eecb5d41625870440da96
unthatile makhajane <unthatile.alx@yahoo.com>
2025-09-14 03:32:03 +0200

    Add 8-square.js to print a square of Xs

---
+++ 8-square.js
@@ -0,0 +1,11 @@
+// 8-square.js
+
+const size = parseInt(process.argv[2]);
+
+if (!size) {
+    console.log("Missing size");
+} else {
+    for (let i = 0; i < size; i++) {
+        console.log("X".repeat(size));
+    }
+}
